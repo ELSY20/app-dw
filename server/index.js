ae5218b9b2618cbab4d82c5d2a3de1e0badd8f94
matosr96 <19matos96@gmail.com>
2022-07-20 14:29:18 -0500

    update

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import actasRoutes from "./routes/actas.routes.js";
+import compromisosRoutes from "./routes/compromisos.routes.js"
 
 const app = express();
 const puerto = 4000;
@@ -8,5 +9,6 @@ app.use(cors());
 app.use(express.json());
 app.listen(puerto);
 app.use(actasRoutes);
+app.use(compromisosRoutes);
 
 console.log("corriendo en el puerto", puerto);
